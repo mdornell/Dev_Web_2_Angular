edb0da27241f8863348cd73c584a365b52357beb
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-09-25 15:58:35 -0300

    ultilizando primeiro componente

--- videoLocadoraAngular/src/app/app.component.ts
+++ videoLocadoraAngular/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from "./main-page/main-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'videoLocadora';
-}
+export class AppComponent {}
