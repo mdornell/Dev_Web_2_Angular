f150037b4c1ce0e6c9d55c28681bfda0e1084a91
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-09-25 16:07:23 -0300

    feat: adicionar MatToobar ao componete main-page

--- videoLocadoraAngular/src/app/main-page/main-page.component.ts
+++ videoLocadoraAngular/src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
