95bc919bf266a429ec0d16e8c4f236692aaf6758
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-07 14:56:34 -0300

    Deletando o back que ta dando erro

--- videoLocadoraAngular/src/app/app.component.ts
+++ videoLocadoraAngular/src/app/app.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AtorService } from '../service/serviceAtor/atorService';
+import { HttpClientModule } from '@angular/common/http';  // Importa o HttpClientModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    HttpClientModule,  
+  ],
+  providers: [AtorService],  // Providencie o serviço no próprio componente autônomo
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private atorService: AtorService) {
+    this.form = this.formBuilder.group({
+      _id: [0],
+      nome: [''],
+    });
+  }
+
+  salvar() {
+    this.atorService.salvar(this.form.value).subscribe(() => {
+      console.log('Ator salvo com sucesso');
+    });
+  }
+}
---
+++ videoLocadoraAngular/src/service/serviceAtor/atorService.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';  // Certifique-se de que os tipos de retorno estejam corretos
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AtorService {
+  private apiUrl = 'http://localhost:3000/ator';  // Defina a URL da API
+
+  constructor(private http: HttpClient) {}
+
+  salvar(ator: FormGroup): Observable<FormGroup> {
+    return this.http.post<FormGroup>(this.apiUrl, ator);  // Passe o objeto `ator` diretamente, não o `FormGroup`
+  }
+}
