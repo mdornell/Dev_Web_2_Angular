d856c3a19998d4de47fcc083ebcd3005212d113d
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-14 20:57:32 -0300

    rm: Divindindo o projeto em dois repositorios

--- videoLocadoraSpring/src/main/java/com/videoLocadora/repository/ClasseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.videoLocadora.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.videoLocadora.domain.controle_acervo.Classe;
-
-public interface ClasseRepository extends JpaRepository<Classe, Long> {
-    
-}
