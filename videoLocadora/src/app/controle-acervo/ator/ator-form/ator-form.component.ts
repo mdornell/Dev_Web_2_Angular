f0769cf56c77992d25f67eb12b5a20450da04fc7
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-16 20:40:38 -0300

    feat: Criando AtorFormComponent

---
+++ videoLocadora/src/app/controle-acervo/ator/ator-form/ator-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ator-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './ator-form.component.html',
+  styleUrl: './ator-form.component.scss'
+})
+export class AtorFormComponent {
+
+}
