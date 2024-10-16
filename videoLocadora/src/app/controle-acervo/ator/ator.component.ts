7441abd9d991291943a9c6c0b791382c0634fdc5
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-16 20:40:07 -0300

    feat: Criando AtorComponent

---
+++ videoLocadora/src/app/controle-acervo/ator/ator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ator',
+  standalone: true,
+  imports: [],
+  templateUrl: './ator.component.html',
+  styleUrl: './ator.component.scss'
+})
+export class AtorComponent {
+
+}
