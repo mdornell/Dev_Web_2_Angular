494368bfb0c552ea725f387520ad03fbac005e29
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-16 20:44:43 -0300

    feat: Criando DiretorListComponent

---
+++ videoLocadora/src/app/controle-acervo/diretor/diretor-list/diretor-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretor-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './diretor-list.component.html',
+  styleUrl: './diretor-list.component.scss'
+})
+export class DiretorListComponent {
+
+}
