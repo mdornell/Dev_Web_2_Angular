dd0e92483061531784550477275e909b66ed5722
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-16 20:43:01 -0300

    feat: Criando ClasseFormComponent

---
+++ videoLocadora/src/app/controle-acervo/classe/classe-form/classe-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classe-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './classe-form.component.html',
+  styleUrl: './classe-form.component.scss'
+})
+export class ClasseFormComponent {
+
+}
