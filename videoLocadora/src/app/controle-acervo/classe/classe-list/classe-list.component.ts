5e54f8295989131f9112b1f6280c83c29bc94b5a
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-16 20:43:21 -0300

    feat: Criando ClasseListComponent

---
+++ videoLocadora/src/app/controle-acervo/classe/classe-list/classe-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classe-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './classe-list.component.html',
+  styleUrl: './classe-list.component.scss'
+})
+export class ClasseListComponent {
+
+}
