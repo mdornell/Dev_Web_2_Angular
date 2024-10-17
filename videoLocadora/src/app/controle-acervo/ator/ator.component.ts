e07d156fdc3ce4e992556d7ecfd4a4b619c35b88
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-17 18:24:47 -0300

    feat: Corrigindo tela Ator / Configurando as rotas para List e Form de ator

--- videoLocadora/src/app/controle-acervo/ator/ator.component.ts
+++ videoLocadora/src/app/controle-acervo/ator/ator.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AtorService } from '../../service/ator/ator.service';
+import { Ator } from '../../type/ator';
+import { AtorListComponent } from './ator-list/ator-list.component';
 
 @Component({
   selector: 'app-ator',
   standalone: true,
-  imports: [],
+  imports: [
+    AtorListComponent,
+    CommonModule,
+  ],
   templateUrl: './ator.component.html',
   styleUrl: './ator.component.scss'
 })
 export class AtorComponent {
 
+    atores$: Observable<Ator[]>;
+
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private atorService: AtorService
+    ) { 
+        this.atores$ = this.atorService.list();
+    }
+
+    ngOnInit() {
+    }
 }
