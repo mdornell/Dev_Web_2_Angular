98d0c7372631f13b5f71559f943a000052c79cf0
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-17 18:22:47 -0300

    feat: Criando tela Diretor/ Fazendo rota para DiretorList

--- videoLocadora/src/app/controle-acervo/diretor/diretor.component.ts
+++ videoLocadora/src/app/controle-acervo/diretor/diretor.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DiretorService } from '../../service/diretor/diretor.service';
+import { Diretor } from '../../type/diretor';
+import { DiretorListComponent } from './diretor-list/diretor-list.component';
 
 @Component({
   selector: 'app-diretor',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    DiretorListComponent
+  ],
   templateUrl: './diretor.component.html',
   styleUrl: './diretor.component.scss'
 })
 export class DiretorComponent {
 
+    diretores$: Observable<Diretor[]>;
+
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private diretorService: DiretorService
+    ) { 
+        this.diretores$ = this.diretorService.list();
+    }
+
+    ngOnInit() {
+    }
 }
