02f7756b4e14a4e056adac46a4099247fc1e137e
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-17 18:23:30 -0300

    feat: Criando tela DiretorList / Configurando recebimento da API

--- videoLocadora/src/app/controle-acervo/diretor/diretor-list/diretor-list.component.ts
+++ videoLocadora/src/app/controle-acervo/diretor/diretor-list/diretor-list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Diretor } from '../../../type/diretor';
 
 @Component({
   selector: 'app-diretor-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgFor
+  ],
   templateUrl: './diretor-list.component.html',
   styleUrl: './diretor-list.component.scss'
 })
 export class DiretorListComponent {
+    
+    @Input() diretores: Diretor[] = []
 
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute
+    ) { }
+
+    ngOnInit() {
+    }
+
+    onAdd() {
+        this.router.navigate(['new'], { relativeTo: this.route });
+    }
+
+    onEdit(diretor: Diretor) {
+        this.router.navigate(['edit', diretor._id], { relativeTo: this.route });
+    }
 }
