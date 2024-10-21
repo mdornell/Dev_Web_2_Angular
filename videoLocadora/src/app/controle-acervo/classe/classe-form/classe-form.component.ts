3cdcb3fb431069eadd73df52774c04924bba2339
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-21 09:18:00 -0300

    feat: Refatorante tela e funcinalidades ClasseFormComponent

--- videoLocadora/src/app/controle-acervo/classe/classe-form/classe-form.component.ts
+++ videoLocadora/src/app/controle-acervo/classe/classe-form/classe-form.component.ts
@@ -1,12 +1,73 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClasseService } from '../../../service/classe/classe.service';
 
 @Component({
-  selector: 'app-classe-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './classe-form.component.html',
-  styleUrl: './classe-form.component.scss'
+    selector: 'app-classe-form',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule,
+    ],
+    templateUrl: './classe-form.component.html',
+    styleUrl: './classe-form.component.scss'
 })
 export class ClasseFormComponent {
 
+    form: FormGroup;
+
+    constructor(
+        private fb: FormBuilder,
+        private classeService: ClasseService,
+        private router: Router,
+        private route: ActivatedRoute,
+        private location: Location,
+        private snackBar: MatSnackBar
+    ) {
+        this.form = this.fb.group({
+            _id: [0],
+            nome: [''],
+            valor: [0,0],
+            dataDeDevolucao: ["dd/mm/aaaa"]
+        });
+     }
+
+     ngOnInit(): void {
+        const classe = this.route.snapshot.data['classe'];
+        this.form.setValue({
+            _id: classe._id,
+            nome: classe.nome,
+            valor: classe.valor,
+            dataDeDevolucao: classe.dataDeDevolucao 
+        });
+    }
+
+    onSubmit() {
+        if (this.form.valid) {
+            this.classeService.save(this.form.value)
+            .subscribe(
+                result => this.onSuccess(),
+                error => this.onErro()
+            );
+        } else {
+            this.snackBar.open('Formulario Invalido', 'X', { duration: 5000 });
+        }
+        
+    }
+
+    onCancel() {
+        this.form.reset();
+        this.location.back();
+    }
+
+    onSuccess() {
+        this.snackBar.open('Registro salvo com sucesso', '', { duration: 5000 });
+        this.location.back();
+    }
+
+    onErro() {
+        this.snackBar.open('Erro ao salvar o registro', '', { duration: 5000 });
+    }
 }
